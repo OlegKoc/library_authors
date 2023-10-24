3698cf50d92cd5e40c0346ee1ca1d7ef73759ae5
OlegKoc <koralll6@yandex.ru>
2023-10-24 22:11:08 +0400

    Initial commit

---
+++ src/main/java/com/example/olegkochurov/always/yes/library_authors/LibraryAuthorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.olegkochurov.always.yes.library_authors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAuthorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAuthorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/olegkochurov/always/yes/library_authors/LibraryAuthorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.olegkochurov.always.yes.library_authors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAuthorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
